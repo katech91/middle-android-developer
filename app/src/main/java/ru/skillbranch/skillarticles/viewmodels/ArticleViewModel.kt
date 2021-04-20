1bb959cf83324adec59fa04f7f2da14d59d6f680
Katerina Chernysheva <katech91@gmail.com>
2021-04-20 13:53:26 +0300

    init

---
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/ArticleViewModel.kt
@@ -0,0 +1,156 @@
+package ru.skillbranch.skillarticles.viewmodels
+
+import androidx.lifecycle.LiveData
+import ru.skillbranch.skillarticles.data.ArticleData
+import ru.skillbranch.skillarticles.data.ArticlePersonalInfo
+import ru.skillbranch.skillarticles.data.repositories.ArticleRepository
+import ru.skillbranch.skillarticles.extensions.data.toAppSettings
+import ru.skillbranch.skillarticles.extensions.data.toArticlePersonalInfo
+import ru.skillbranch.skillarticles.extensions.format
+
+class ArticleViewModel(private val articleId: String): BaseViewModel<ArticleState>(ArticleState()),
+    IArticleViewModel {
+    private val repository = ArticleRepository
+    init {
+        //subscribe on mutable data
+        subscribeOnDataSource(getArticleData()){ article, state ->
+            article ?: return@subscribeOnDataSource null
+            state.copy(
+                    shareLink = article.shareLink,
+                    title = article.title,
+                    category = article.category,
+                    categoryIcon = article.categoryIcon,
+                    date = article.date.format()
+            )
+        }
+
+        subscribeOnDataSource(getArticleContent()){ content, state ->
+            content ?: return@subscribeOnDataSource null
+            state.copy(
+                    isLoadingContent = false,
+                    content = content
+            )
+        }
+
+        subscribeOnDataSource(getArticlePersonalInfo()){ info, state ->
+            info ?: return@subscribeOnDataSource null
+            state.copy(
+                    isBookmark = info.isBookmark,
+                    isLike = info.isLike
+            )
+        }
+
+        subscribeOnDataSource(repository.getAppSettings()){ settings, state ->
+            state.copy(
+                    isDarkMode = settings.isDarkMode,
+                    isBigText = settings.isBigText
+            )
+        }
+    }
+
+    //load text from network
+    override fun getArticleContent(): LiveData<List<Any>?> {
+        return repository.loadArticleContent(articleId)
+    }
+
+    //load data from db
+    override fun getArticleData(): LiveData<ArticleData?> {
+        return repository.getArticle(articleId)
+    }
+
+    //load data from db
+    override fun getArticlePersonalInfo(): LiveData<ArticlePersonalInfo?> {
+        return repository.loadArticlePersonalInfo(articleId)
+    }
+
+    override fun handleUpText() {
+        repository.updateSettings(currentState.toAppSettings().copy(isBigText = true))
+    }
+
+    override fun handleDownText() {
+        repository.updateSettings(currentState.toAppSettings().copy(isBigText = false))
+    }
+
+    //app setings
+    override fun handleNightMode() {
+        val settings = currentState.toAppSettings()
+        repository.updateSettings(settings.copy(isDarkMode = !settings.isDarkMode))
+    }
+
+    override fun handleLike() {
+        val toggleLike = {
+            val info = currentState.toArticlePersonalInfo()
+            repository.updateArticlePersonalInfo(info.copy(isLike = !info.isLike))
+        }
+
+        toggleLike()
+        val msg = if (currentState.isLike) Notify.TextMessage("Mark as liked")
+        else {
+            Notify.ActionMessage(
+                    "Don't like it anymore", //message
+                    "No, still like it", //action label
+                    toggleLike
+            )
+        }
+
+        notify(msg)
+    }
+
+    override fun handleBookmark() {
+        val toggleBookmark = {
+            val info = currentState.toArticlePersonalInfo()
+            repository.updateArticlePersonalInfo(info.copy(isBookmark = !info.isBookmark))
+        }
+
+        toggleBookmark()
+        val msg = if(currentState.isBookmark) {
+            Notify.TextMessage("Add to bookmarks")
+        }else {
+            Notify.TextMessage("Remove from bookmarks")
+        }
+
+        notify(msg)
+    }
+
+    override fun handleShare() {
+        val msg = "Share is not implemented"
+        notify(Notify.ErrorMessage(msg, "OK", null))
+    }
+
+    //session state
+    override fun handleToggleMenu() {
+        updateState { it.copy(isShowMenu = !it.isShowMenu) }
+    }
+
+    override fun handleSearchMode(isSearch: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun handleSearch(query: String?) {
+        TODO("Not yet implemented")
+    }
+}
+
+data class ArticleState (
+        val isAuth: Boolean = false,
+        val isLoadingContent: Boolean = true,
+        val isLoadingReviews: Boolean = true,
+        val isLike: Boolean = false,
+        val isBookmark: Boolean = false,
+        val isShowMenu: Boolean = false,
+        val isBigText: Boolean = false,
+        val isDarkMode: Boolean = false,
+        val isSearch: Boolean = false,
+        val searchQuery: String? = null,
+        val searchResults: List<Pair<Int, Int>> = emptyList(),
+        val searchPosition: Int = 0,
+        val shareLink: String? = null,
+        val title: String? = null,
+        val category: String? = null,
+        val categoryIcon: Any? = null,
+        val date: String? = null,
+        val author: Any? = null,
+        val poster: String? = null,
+        val content: List<Any> = emptyList(),
+        val reviews: List<Any> = emptyList()
+)
---
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/BaseViewModel.kt
@@ -0,0 +1,105 @@
+package ru.skillbranch.skillarticles.viewmodels
+
+import androidx.annotation.UiThread
+import androidx.lifecycle.*
+import java.lang.IllegalArgumentException
+
+abstract class BaseViewModel<T>(initState: T) : ViewModel() {
+    protected val notifications = MutableLiveData<Event<Notify>>()
+
+    protected val state: MediatorLiveData<T> = MediatorLiveData<T>().apply {
+        value = initState
+    }
+
+    //not null current state
+    protected val currentState
+        get() = state.value!!
+
+    /***
+    * Лямбда-выражение принимает в качестве в качестве аргумента лямбду, в которую предается текущее состояние
+    * и она возвращает модифицированное состояние, которое присваивается текущему состоянию
+    */
+    @UiThread
+    protected inline fun updateState(update:(currentState: T) -> T) {
+        val updatedState: T = update(currentState)
+        state.value = updatedState
+    }
+
+    @UiThread
+    protected fun notify(content: Notify) {
+        notifications.value = Event(content)
+    }
+
+    /***
+    * более компактная форма записи observe принимает последним аргументом лямбда выражение,
+    * обрабатывающее изменение текущего состояния
+    */
+    fun observeState(owner: LifecycleOwner, onChanged: (newState: T) -> Unit) {
+        state.observe(owner, Observer { onChanged(it!!) })
+    }
+
+    /***
+    * более компактная форма записи observe вызывает лямбда выражение - обработчик только в том случае,
+    * если сообщение не было уже обработано, реализует данное поведение благодаря EventObserver
+    */
+    fun observeNotifications(owner: LifecycleOwner, onNotify: (notification: Notify)->Unit){
+        notifications.observe(owner, EventObserver { onNotify(it) })
+    }
+
+    protected fun <S> subscribeOnDataSource(
+            source: LiveData<S>,
+            onChanged: (newValue: S, currentState: T) -> T?
+    ){
+        state.addSource(source){
+            state.value = onChanged(it, currentState) ?:  return@addSource
+        }
+    }
+}
+
+class ViewModelFactory(private val params: String): ViewModelProvider.Factory {
+    override fun <T: ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ArticleViewModel::class.java)) {
+            return ArticleViewModel(params) as T
+        }
+        throw IllegalArgumentException("Anknown view model class")
+    }
+}
+
+class Event<out E>(private val content: E) {
+    var hasBeenHandled = false
+
+    fun  getContentIfNotHandled(): E?{
+        return if (hasBeenHandled) null
+        else {
+            hasBeenHandled = true
+            content
+        }
+    }
+}
+
+class EventObserver<E>(private val onEventUnhandledContent: (E) -> Unit): Observer<Event<E>>{
+    //в качестве аргумента принимает лямбда выражение - обработчик, в который передается
+    // необработанное ранее событие, получаемое в реализации метода Observer'а OnChanged
+    override fun onChanged(event: Event<E>?) {
+        //если есть необработанное сообщение (контент), передай в лямбду в качестве аргумента
+        event?.getContentIfNotHandled()?.let {
+            onEventUnhandledContent(it)
+        }
+    }
+}
+
+sealed class Notify(val message: String) {
+    data class TextMessage(val msg: String): Notify(msg)
+
+    data class ActionMessage(
+            val msg: String,
+            val actionLabel: String,
+            val actionHandler: (()->Unit)?
+    ): Notify(msg)
+
+    data class ErrorMessage(
+            val msg: String,
+            val errLabel: String,
+            val errHandler: (()->Unit)?
+    ): Notify(msg)
+}
